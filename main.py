5e24226d44940604216c3f7bbb64038ad5af4656
am1t-kumar <thorragnarak@gmail.com>
2024-06-02 11:55:03 +0530

    add: crud apis

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000",
-    "localhost:3000",
+    "http://localhost:5173",
+    "localhost:5173",
 ]
 
 app.add_middleware(
@@ -20,3 +20,43 @@ app.add_middleware(
 @app.get("/")
 async def root() -> dict:
     return {"message": "Hello World!"}
+
+
+books = [
+    {"id": 1, "title": "Harry Potter", "author": "J.K. Rowling", "year": 1997},
+    {
+        "id": 2,
+        "title": "The Lord of the Rings",
+        "author": "J.R.R. Tolkien",
+        "year": 1954,
+    },
+]
+
+
+@app.get("/books")
+async def get_books() -> dict:
+    return {"books": books}
+
+
+@app.post("/books")
+async def add_book(book: dict) -> dict:
+    books.append(book)
+    return {"books": books, "message": "Book added successfully!"}
+
+
+@app.delete("/books/{book_id}")
+async def delete_book(book_id: int) -> dict:
+    for book in books:
+        if book["id"] == book_id:
+            books.remove(book)
+            return {"books": books, "message": "Book deleted successfully!"}
+    return {"message": "Book not found!"}
+
+
+@app.put("/books/{book_id}")
+async def update_book(book_id: int, book: dict) -> dict:
+    for b in books:
+        if b["id"] == book_id:
+            b.update(book)
+            return {"books": books, "message": "Book updated successfully!"}
+    return {"message": "Book not found!"}
